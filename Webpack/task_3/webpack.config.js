c632b028c07535efdba68f2ceccb84c119b13636
ScandereT78 <scanderetejpro@gmail.com>
2025-09-18 13:15:33 +0200

    test jespere que sa marche ahahahaha

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,45 +1,45 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    header: './modules/header/header.js',
-    body: './modules/body/body.js',
-    footer: './modules/footer/footer.js'
+    header: "./modules/header/header.js",
+    body: "./modules/body/body.js",
+    footer: "./modules/footer/footer.js",
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public')
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  devServer: {
+    static: path.resolve(__dirname, "public"),
+    port: 8564,
+    open: true,
   },
   module: {
     rules: [
-      { 
-        test: /\.css$/i, 
-        use: ['style-loader', 'css-loader'] 
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
-      { 
-        test: /\.(png|jpg|jpeg|gif)$/i, 
-        type: 'asset/resource' 
-      }
-    ]
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: path.resolve(__dirname, 'public'),
-    port: 8564,
-    open: true
+  devtool: "inline-source-map",
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Holberton Dashboard'
-    })
+      title: "Holberton Dashboard",
+    }),
   ],
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
-  }
 };
