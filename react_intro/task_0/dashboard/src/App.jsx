c49a0a9c00c4623f043b4dceb421b99109eaa14f
ScandereT78 <scanderetejpro@gmail.com>
2025-09-22 01:10:19 +0200

    fix app.jsx

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -5,20 +5,20 @@ function App() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <>
-    <div className='App-header'>
-      <img src={holbertonLogo} alt="holberton logo" />
-      <h1>School dashboard</h1>
+    <div>
+      <div className='App-header'>
+        <img src={holbertonLogo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+      
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+      
+      <div className='App-footer'>
+        <p>Copyright {currentYear} - Holberton School</p>
+      </div>
     </div>
-
-    <div className='App-body'>
-      <p>Login to access the full dashboard</p>
-    </div>
-
-    <div className='App-footer'>
-      <p>Copyright {currentYear} - Holberton School</p>
-    </div>
-    </>
   )
 }
 
