5535b86d45a60112dcdaa2c02a0d33c542426e24
ScandereT78 <scanderetejpro@gmail.com>
2025-09-22 16:23:27 +0200

    fixtask 0

--- react_intro/task_0/dashboard/eslint.config.js
+++ react_intro/task_0/dashboard/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(["dist", "babel.config.js", "setupTests.js", "testRunner.js", "test_0.spec.js", "fileTransformer.js"]),
   {
     files: ["**/*.{js,jsx}"],
     extends: [
