90865d10ea854bb1b852c34496919e43570d16ec
ScandereT78 <scanderetejpro@gmail.com>
2025-09-22 14:56:34 +0200

    fix

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,25 +1,26 @@
-import holbertonLogo from './assets/holberton-logo.jpg'
-import './App.css'
+import React from "react";
+import './App.css';
+import logo from './assets/holberton-logo.jpg';
 
 function App() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="App">
-      <div className='App-header'>
-        <img src={holbertonLogo} alt="holberton logo" />
+    <>
+      <div className="App-header">
+        <img src={logo} alt="holberton logo" />
         <h1>School dashboard</h1>
       </div>
-      
-      <div className='App-body'>
+
+      <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-      
-      <div className='App-footer'>
-        <p>Copyright {currentYear} - Holberton School</p>
+
+      <div className="App-footer">
+        <p>Copyright {currentYear} - holberton School</p>
       </div>
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
