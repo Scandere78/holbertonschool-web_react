00f33e0b13cad274514ffdcb53765be3281b3f33
ScandereT78 <scanderetejpro@gmail.com>
2025-09-22 15:50:46 +0200

    2. Embedding expressions, functions

---
+++ react_intro/task_1/dashboard/src/Notifications.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './Notifications.css';
+
+function Notifications() {
+  return (
+    <div className="notification-items">
+      <p>Here is the list of notifications</p>
+    </div>
+  );
+}
+
+export default Notifications;
