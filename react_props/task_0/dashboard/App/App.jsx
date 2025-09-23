ae06f24fe3d1feae8cf69d410928055a80d71420
ScandereT78 <scanderetejpro@gmail.com>
2025-09-23 12:52:29 +0200

    fix structure

--- react_props/task_0/dashboard/App/App.jsx
+++ react_props/task_0/dashboard/App/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import './App.css';
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
-import Login from './Login/Login';
+import Header from '../src/Header/Header';
+import Footer from '../src/Footer/Footer';
+import Login from '../src/Login/Login';
 
 
 function App() {
