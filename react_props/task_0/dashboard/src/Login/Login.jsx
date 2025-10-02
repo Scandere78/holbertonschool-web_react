a04f33ed7b49c821aada35d5bf84d0e7ea210fbc
ScandereT78 <scanderetejpro@gmail.com>
2025-10-02 06:42:59 +0100

    fix task 0

--- react_props/task_0/dashboard/src/Login/Login.jsx
+++ react_props/task_0/dashboard/src/Login/Login.jsx
@@ -7,6 +7,15 @@ function Login() {
     <>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+          
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+          
+          <button type="submit">OK</button>
+        </form>
       </div>
     </>
   );
