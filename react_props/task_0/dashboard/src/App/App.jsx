2c3d95f358a0061e4645de7e7e813627ba192d4c
ScandereT78 <scanderetejpro@gmail.com>
2025-10-07 15:34:40 +0100

    fix

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -1,22 +1,24 @@
-import React, { Fragment } from 'react';
-import './App.css';
+// src/App/App.jsx
+import React, { Fragment } from "react";
+import "./App.css";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
 
-import Notifications from '../Notifications/Notifications';
-import Header from '../Header/Header';
-import Login from '../Login/Login';
-import Footer from '../Footer/Footer';
-
-function App() {
-  return (
-    <Fragment>
-      <div className="root-notifications">
-        <Notifications />
-      </div>
-      <Header />
-      <Login />
-      <Footer />
-    </Fragment>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="root-notifications">
+          <Notifications />
+        </div>
+        <Header />
+        <Login />
+        <Footer />
+      </Fragment>
+    );
+  }
 }
 
 export default App;
